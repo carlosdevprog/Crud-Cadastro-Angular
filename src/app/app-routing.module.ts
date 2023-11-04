aa61bfa164f73cdceb9a76a72da793f0d0dea2a9
Carlos Eduardo Lima <carloseduardolima.dev@gmail.com>
2023-11-04 20:37:58 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'funcionarios', component: EmployeeListComponent },
+  { path: '', redirectTo: 'funcionarios', pathMatch: 'full' },
+  { path: 'criar-funcionario', component: CreateEmployeeComponent },
+  { path: 'atualizar-funcionario/:id', component: UpdateEmployeeComponent },
+  { path: 'ver-funcionario/:id', component: EmployeeDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from './../employee.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css'],
+})
+export class CreateEmployeeComponent {
+  employee: Employee = new Employee();
+
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
+
+  goToEmployeeList() {
+    this.router.navigate(['/funcionarios']);
+  }
+
+  saveEmployee() {
+    this.employeeService.createEmployee(this.employee).subscribe(
+      (data) => {
+        console.log(data);
+        this.goToEmployeeList();
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  onSubmit() {
+    console.log(this.employee);
+    this.saveEmployee();
+
+    //Delete inputs after click
+    this.employee.firstName = '';
+    this.employee.lastName = '';
+    this.employee.email = '';
+  }
+}
